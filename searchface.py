2bc36804db207ccd1d75df71f9951a1701fe945c
d4n4i <37236863+d4n4i@users.noreply.github.com>
2018-06-03 19:20:06 +0300

    initial commit

--- searchface.py
+++ searchface.py
@@ -3,7 +3,7 @@ import const
 import telegram as tg
 import telegram.ext as tgext
 import skimage
-# from skimage import io
+from skimage import io
 # from skimage.io import imread
 # import skimage.data
 from skimage.feature import hog
@@ -16,7 +16,7 @@ people_size = (62, 47)
 
 
 def search(filepath):
-    img = cv2.imread(filepath)
+    img = skimage.imread(filepath)
     rects = list()
     print(img.shape)
     print(type(img))
